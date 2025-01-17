932e15b8f35c318043636818f7ce62caa595f67b
CountJung <ultimalife@naver.com>
2025-01-17 16:55:42 +0900

    초기 커밋.

---
+++ AvaloniaWebDeploySample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaWebDeploySample.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AvaloniaWebDeploySample/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaWebDeploySample.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
